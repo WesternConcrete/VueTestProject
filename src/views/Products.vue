4a55125509bfca477d009de7ea449c3cab9a7b84
Wes Convery <2wconvery@gmail.com>
2022-01-22 03:36:16 -0800

    posting project

---
+++ src/views/Products.vue
@@ -0,0 +1,26 @@
+<template>
+    <main class="wrapper">
+        <h1>Products</h1>
+        <div class="card-container">
+            <ProductCard
+                v-for="product in inventory"
+                :key="product.id"
+                class="card"
+                :product="product"
+                :addToCart="addToCart"
+            />
+        </div>
+    </main>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+
+export default {
+  name: 'Products',
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+</script>
